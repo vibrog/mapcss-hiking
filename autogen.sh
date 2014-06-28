9ea53f310439416979f7869a8865202fbc584cd1
Vidar Gundersen <github.vbg@spamgourmet.org>
2014-06-28 08:56:46 +0200

    Include line features style in build script

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 cat meta.css > style.mapcss
 grep -h -v '^meta' \
- landscape.mapcss roads.mapcss \
+ landscape.mapcss linefeatures.mapcss roads.mapcss \
  trailvisibility.mapcss routes.mapcss \
  buildings.mapcss symbols.mapcss \
  ski-nordic.mapcss ski-alpine.mapcss \
  hidenodes.css \
  >> style.mapcss
-#java -jar /Library/Java/yuicompressor-2.4.jar --type css \
-# -o style.min.mapcss style.mapcss
-#mv style.min.mapcss style.mapcss
 zip -r mapcss-hiking.zip style.mapcss readme.md symbols/[a-z]*.svg
